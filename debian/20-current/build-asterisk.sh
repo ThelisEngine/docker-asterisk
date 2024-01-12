478245be898278e025a04f09bd926eb7fd9e2066
Cédric Jadot <cjadot@thelis.be>
2024-01-12 14:16:57 +0100

    debian bookworm (12) version with Asterisk 20 added

---
+++ debian/20-current/build-asterisk.sh
@@ -0,0 +1,225 @@
+#!/usr/bin/env bash
+
+PROGNAME=$(basename $0)
+
+if test -z ${ASTERISK_VERSION}; then
+  echo "${PROGNAME}: ASTERISK_VERSION required" >&2
+  exit 1
+fi
+
+set -ueo pipefail
+
+useradd --system asterisk
+
+DEBIAN_FRONTEND=noninteractive \
+apt-get update -qq
+
+DEBIAN_FRONTEND=noninteractive \
+apt-get install --yes -qq --no-install-recommends --no-install-suggests \
+  autoconf \
+  binutils-dev \
+  build-essential \
+  ca-certificates \
+  curl \
+  file \
+  libcurl4-openssl-dev \
+  libedit-dev \
+  libgsm1-dev \
+  libogg-dev \
+  libpopt-dev \
+  libresample1-dev \
+  libspandsp-dev \
+  libspeex-dev \
+  libspeexdsp-dev \
+  libsqlite3-dev \
+  libsrtp2-dev \
+  libssl-dev \
+  libvorbis-dev \
+  libxml2-dev \
+  libxslt1-dev \
+  odbcinst \
+  portaudio19-dev \
+  procps \
+  unixodbc \
+  unixodbc-dev \
+  uuid \
+  uuid-dev \
+  xmlstarlet \
+  subversion \
+> /dev/null
+
+DEBIAN_FRONTEND=noninteractive \
+apt-get purge --yes -qq --auto-remove > /dev/null
+rm -rf /var/lib/apt/lists/*
+
+mkdir -p /usr/src/asterisk
+cd /usr/src/asterisk
+
+( \
+  curl -sL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
+  curl -sL http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz || \
+  curl -sL http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-${ASTERISK_VERSION}.tar.gz | tar --strip-components 1 -xz \
+) &>/dev/null
+
+# 1.5 jobs per core works out okay
+: ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
+
+./configure --with-resample \
+            --with-pjproject-bundled \
+            --with-jansson-bundled > /dev/null
+make menuselect/menuselect menuselect-tree menuselect.makeopts
+
+# disable BUILD_NATIVE to avoid platform issues
+menuselect/menuselect --disable BUILD_NATIVE menuselect.makeopts
+
+# enable good things
+menuselect/menuselect --enable BETTER_BACKTRACES menuselect.makeopts
+
+# enable ooh323
+menuselect/menuselect --enable chan_ooh323 menuselect.makeopts
+
+# codecs
+# menuselect/menuselect --enable codec_opus menuselect.makeopts
+# menuselect/menuselect --enable codec_silk menuselect.makeopts
+
+# # download more sounds
+# for i in CORE-SOUNDS-EN MOH-OPSOUND EXTRA-SOUNDS-EN; do
+#   for j in ULAW ALAW G722 GSM SLN16; do
+#     menuselect/menuselect --enable $i-$j menuselect.makeopts
+#   done
+# done
+
+contrib/scripts/get_mp3_source.sh
+
+# we don't need any sounds in docker, they will be mounted as volume
+menuselect/menuselect --disable-category MENUSELECT_CORE_SOUNDS menuselect.makeopts
+menuselect/menuselect --disable-category MENUSELECT_MOH menuselect.makeopts
+menuselect/menuselect --disable-category MENUSELECT_EXTRA_SOUNDS menuselect.makeopts
+menuselect/menuselect menuselect/menuselect \
+  --disable BUILD_NATIVE \
+  --enable format_mp3 \
+  --enable cdr_csv \
+  --enable chan_sip \
+  --enable res_snmp \
+  --enable res_http_websocket \
+  --enable res_hep_pjsip \
+  --enable res_hep_rtcp \
+  --enable res_sorcery_astdb \
+  --enable res_sorcery_config \
+  --enable res_sorcery_memory \
+  --enable res_sorcery_memory_cache \
+  --enable res_pjproject \
+  --enable res_rtp_asterisk \
+  --enable res_ari \
+  --enable res_ari_applications \
+  --enable res_ari_asterisk \
+  --enable res_ari_bridges \
+  --enable res_ari_channels \
+  --enable res_ari_device_states \
+  --enable res_ari_endpoints \
+  --enable res_ari_events \
+  --enable res_ari_mailboxes \
+  --enable res_ari_model \
+  --enable res_ari_playbacks \
+  --enable res_ari_recordings \
+  --enable res_ari_sounds \
+  --enable res_pjsip \
+  --enable res_pjsip_acl \
+  --enable res_pjsip_authenticator_digest \
+  --enable res_pjsip_caller_id \
+  --enable res_pjsip_config_wizard \
+  --enable res_pjsip_dialog_info_body_generator \
+  --enable res_pjsip_diversion \
+  --enable res_pjsip_dlg_options \
+  --enable res_pjsip_dtmf_info \
+  --enable res_pjsip_empty_info \
+  --enable res_pjsip_endpoint_identifier_anonymous \
+  --enable res_pjsip_endpoint_identifier_ip \
+  --enable res_pjsip_endpoint_identifier_user \
+  --enable res_pjsip_exten_state \
+  --enable res_pjsip_header_funcs \
+  --enable res_pjsip_logger \
+  --enable res_pjsip_messaging \
+  --enable res_pjsip_mwi \
+  --enable res_pjsip_mwi_body_generator \
+  --enable res_pjsip_nat \
+  --enable res_pjsip_notify \
+  --enable res_pjsip_one_touch_record_info \
+  --enable res_pjsip_outbound_authenticator_digest \
+  --enable res_pjsip_outbound_publish \
+  --enable res_pjsip_outbound_registration \
+  --enable res_pjsip_path \
+  --enable res_pjsip_pidf_body_generator \
+  --enable res_pjsip_publish_asterisk \
+  --enable res_pjsip_pubsub \
+  --enable res_pjsip_refer \
+  --enable res_pjsip_registrar \
+  --enable res_pjsip_rfc3326 \
+  --enable res_pjsip_sdp_rtp \
+  --enable res_pjsip_send_to_voicemail \
+  --enable res_pjsip_session \
+  --enable res_pjsip_sips_contact \
+  --enable res_pjsip_t38 \
+  --enable res_pjsip_transport_websocket \
+  --enable res_pjsip_xpidf_body_generator \
+  --enable res_statsd \
+  --enable res_timing_timerfd \
+  --enable res_stasis \
+  --enable res_stasis_answer \
+  --enable res_stasis_device_state \
+  --enable res_stasis_mailbox \
+  --enable res_stasis_playback \
+  --enable res_stasis_recording \
+  --enable res_stasis_snoop \
+  --enable res_stasis_test \
+  menuselect.makeopts
+
+
+make -j ${JOBS} all > /dev/null || make -j ${JOBS} all
+make install > /dev/null
+
+# copy default configs
+# cp /usr/src/asterisk/configs/basic-pbx/*.conf /etc/asterisk/
+make samples > /dev/null
+
+# set runuser and rungroup
+sed -i -E 's/^;(run)(user|group)/\1\2/' /etc/asterisk/asterisk.conf
+
+# Install opus, for some reason menuselect option above does not working
+mkdir -p /usr/src/codecs/opus
+cd /usr/src/codecs/opus
+curl -sL http://downloads.digium.com/pub/telephony/codec_opus/${OPUS_CODEC}.tar.gz | tar --strip-components 1 -xz
+cp *.so /usr/lib/asterisk/modules/
+cp codec_opus_config-en_US.xml /var/lib/asterisk/documentation/
+
+mkdir -p /etc/asterisk/ \
+         /var/spool/asterisk/fax
+
+chown -R asterisk:asterisk /etc/asterisk \
+                           /var/*/asterisk \
+                           /usr/*/asterisk
+chmod -R 750 /var/spool/asterisk
+
+cd /
+rm -rf /usr/src/asterisk \
+       /usr/src/codecs
+
+DEVPKGS="$(dpkg -l | grep '\-dev' | awk '{print $2}' | xargs)"
+DEBIAN_FRONTEND=noninteractive apt-get --yes -qq purge \
+  autoconf \
+  build-essential \
+  bzip2 \
+  cpp \
+  m4 \
+  make \
+  patch \
+  perl \
+  perl-modules \
+  pkg-config \
+  xz-utils \
+  ${DEVPKGS} \
+> /dev/null
+
+rm -rf /var/lib/apt/lists/*
+
+exec rm -f /build-asterisk.sh
