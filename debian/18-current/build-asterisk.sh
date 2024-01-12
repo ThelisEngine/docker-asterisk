580cc4710a442815eb127ad43739620bb03cdb15
Cédric Jadot <cjadot@thelis.be>
2024-01-12 09:35:11 +0100

    Fix line encoding + missing modules for Thello

--- debian/18-current/build-asterisk.sh
+++ debian/18-current/build-asterisk.sh
@@ -45,6 +45,7 @@ apt-get install --yes -qq --no-install-recommends --no-install-suggests \
   uuid \
   uuid-dev \
   xmlstarlet \
+  subversion \
 > /dev/null
 
 DEBIAN_FRONTEND=noninteractive \
@@ -88,10 +89,91 @@ menuselect/menuselect --enable chan_ooh323 menuselect.makeopts
 #   done
 # done
 
+contrib/scripts/get_mp3_source.sh
+
 # we don't need any sounds in docker, they will be mounted as volume
 menuselect/menuselect --disable-category MENUSELECT_CORE_SOUNDS menuselect.makeopts
 menuselect/menuselect --disable-category MENUSELECT_MOH menuselect.makeopts
 menuselect/menuselect --disable-category MENUSELECT_EXTRA_SOUNDS menuselect.makeopts
+menuselect/menuselect menuselect/menuselect \
+  --disable BUILD_NATIVE \
+  --enable format_mp3 \
+  --enable cdr_csv \
+  --enable chan_sip \
+  --enable res_snmp \
+  --enable res_http_websocket \
+  --enable res_hep_pjsip \
+  --enable res_hep_rtcp \
+  --enable res_sorcery_astdb \
+  --enable res_sorcery_config \
+  --enable res_sorcery_memory \
+  --enable res_sorcery_memory_cache \
+  --enable res_pjproject \
+  --enable res_rtp_asterisk \
+  --enable res_ari \
+  --enable res_ari_applications \
+  --enable res_ari_asterisk \
+  --enable res_ari_bridges \
+  --enable res_ari_channels \
+  --enable res_ari_device_states \
+  --enable res_ari_endpoints \
+  --enable res_ari_events \
+  --enable res_ari_mailboxes \
+  --enable res_ari_model \
+  --enable res_ari_playbacks \
+  --enable res_ari_recordings \
+  --enable res_ari_sounds \
+  --enable res_pjsip \
+  --enable res_pjsip_acl \
+  --enable res_pjsip_authenticator_digest \
+  --enable res_pjsip_caller_id \
+  --enable res_pjsip_config_wizard \
+  --enable res_pjsip_dialog_info_body_generator \
+  --enable res_pjsip_diversion \
+  --enable res_pjsip_dlg_options \
+  --enable res_pjsip_dtmf_info \
+  --enable res_pjsip_empty_info \
+  --enable res_pjsip_endpoint_identifier_anonymous \
+  --enable res_pjsip_endpoint_identifier_ip \
+  --enable res_pjsip_endpoint_identifier_user \
+  --enable res_pjsip_exten_state \
+  --enable res_pjsip_header_funcs \
+  --enable res_pjsip_logger \
+  --enable res_pjsip_messaging \
+  --enable res_pjsip_mwi \
+  --enable res_pjsip_mwi_body_generator \
+  --enable res_pjsip_nat \
+  --enable res_pjsip_notify \
+  --enable res_pjsip_one_touch_record_info \
+  --enable res_pjsip_outbound_authenticator_digest \
+  --enable res_pjsip_outbound_publish \
+  --enable res_pjsip_outbound_registration \
+  --enable res_pjsip_path \
+  --enable res_pjsip_pidf_body_generator \
+  --enable res_pjsip_publish_asterisk \
+  --enable res_pjsip_pubsub \
+  --enable res_pjsip_refer \
+  --enable res_pjsip_registrar \
+  --enable res_pjsip_rfc3326 \
+  --enable res_pjsip_sdp_rtp \
+  --enable res_pjsip_send_to_voicemail \
+  --enable res_pjsip_session \
+  --enable res_pjsip_sips_contact \
+  --enable res_pjsip_t38 \
+  --enable res_pjsip_transport_websocket \
+  --enable res_pjsip_xpidf_body_generator \
+  --enable res_statsd \
+  --enable res_timing_timerfd \
+  --enable res_stasis \
+  --enable res_stasis_answer \
+  --enable res_stasis_device_state \
+  --enable res_stasis_mailbox \
+  --enable res_stasis_playback \
+  --enable res_stasis_recording \
+  --enable res_stasis_snoop \
+  --enable res_stasis_test \
+  menuselect.makeopts
+
 
 make -j ${JOBS} all > /dev/null || make -j ${JOBS} all
 make install > /dev/null
